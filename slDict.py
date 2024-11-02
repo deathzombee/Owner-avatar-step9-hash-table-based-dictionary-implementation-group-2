5ceecb4e7ff417c79e5b6e4bfc69b6f8971181c2
Lisa Miao <142939954+Miao-Lisa@users.noreply.github.com>
2024-11-02 03:41:54 +0000

    adding 3 files worked on as a group.

---
+++ slDict.py
@@ -0,0 +1,22 @@
+from dictabs import DictAbstract
+from pairD import PairD
+
+
+class SLDict(DictAbstract):
+    def sort():
+        pass
+
+    def __len__(self):
+        pass
+
+    def __contains__(self, key):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def pop(self, key):
+        pass
